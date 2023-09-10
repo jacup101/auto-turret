5d63a82a595ff56ea2dee4887439794d92cd1e09
jacup101 <jacup105@gmail.com>
2023-09-10 16:11:37 -0700

    First commit, with working functionality using one model

---
+++ kk_arduino.ino
@@ -0,0 +1,35 @@
+bool spin;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  spin = false;
+  Serial.begin(9600);
+}
+
+void pulse(int pulse_dur) {
+  if (!spin) {
+    return;
+  }
+  digitalWrite(7, HIGH);
+  delay(pulse_dur);
+  digitalWrite(7, LOW);
+  delay(pulse_dur);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if (Serial.available()) {
+      int dir = Serial.read();
+      if (dir == 'l') {
+        digitalWrite(8, HIGH);
+        spin = true;
+      } else if (dir == 'r') {
+        digitalWrite(8, LOW);
+        spin = true;
+      } else {
+        spin = false;
+      }
+    }
+    pulse(5);
+}
